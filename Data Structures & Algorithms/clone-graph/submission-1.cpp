dc360f2e43b69687e98ec4f2f3bece448801151d
Harshit Malpani <65167938+hmalpani@users.noreply.github.com>
2026-09-11 14:03:01 -0400

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return nullptr;
+        }
+        unordered_map<Node*, Node*> mp;
+        queue<Node*> q;
+        mp[node] = new Node(node->val);
+        q.push(node);
+
+        while (!q.empty()) {
+            Node* cur = q.front();
+            q.pop();
+            for (Node* n: cur->neighbors) {
+                if (!mp.contains(n)) {
+                    mp[n] = new Node(n->val);
+                    q.push(n);
+                }
+                mp[cur]->neighbors.push_back(mp[n]);
+            }
+        }
+        return mp[node];
+    }
+};
