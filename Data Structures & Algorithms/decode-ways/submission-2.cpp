1f859e163d75264bec724055081147bb46b57f9c
Harshit Malpani <65167938+hmalpani@users.noreply.github.com>
2026-09-15 15:45:56 -0400

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        if (s[n - 1] != '0') {
+            dp[n - 1] = 1;
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            if (s[i] == '0') continue;
+            dp[i] = dp[i + 1];
+            int num = ((s[i] - '0') * 10) + (s[i + 1] - '0');
+            cout<<num;
+            if (num <= 26) {
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+};
